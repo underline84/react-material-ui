d818d9fb9230f9b51ef83f0d9b997d6ef5b77b10
underline84 <evanfatu@hotmail.com>
2022-09-20 21:17:18 -0300

    Iniciando pagina central

---
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -0,0 +1,43 @@
+import { Icon, IconButton, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useDrawerContext } from "../contexts";
+
+interface ILayoutBaseDePaginaProps {
+    titulo: string;
+    children: React.ReactNode;
+}
+
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo }) => {
+    const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const { toggleDrawerOpen } = useDrawerContext();
+
+    return (
+
+        <Box height="100%" display="flex" flexDirection="column" gap={1}>
+            <Box padding={1} display="flex" alignItems="center" gap={1} height={theme.spacing(12)}>
+                {smDown && 
+                <IconButton onClick={toggleDrawerOpen}>
+                    <Icon>menu</Icon>
+                </IconButton>}
+
+                <Typography variant="h5">
+                    {titulo}
+                </Typography>
+
+            </Box>
+
+            <Box>
+                Barra de ferrametas
+            </Box>
+
+            <Box>
+                {children}
+            </Box>
+
+        </Box>
+
+    );
+};
